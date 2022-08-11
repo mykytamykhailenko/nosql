189383fa25ec5d68a29df2ec9ffeac98d405edc2
Mykyta Mykhailenko <mykyta.mykhailenko@aimprosoft.com>
2022-08-11 17:47:35 +0300

    refactor: Uncommented some code.

--- doobie/app/com/aimprosoft/doobie/module/PlayModule.scala
+++ doobie/app/com/aimprosoft/doobie/module/PlayModule.scala
@@ -9,6 +9,7 @@ import com.aimprosoft.doobie.helpers.GetFields._
 import com.aimprosoft.doobie.inter.DoobieBasicOpInter.DoobieActionLang
 import com.aimprosoft.doobie.inter.{IOMatLang, TaskMatLang, ZIOMatLang}
 import com.google.inject.{AbstractModule, TypeLiteral}
+import monix.eval.Task
 
 import scala.concurrent.ExecutionContext.{global => globalContext}
 import monix.execution.Scheduler.Implicits.{global => globalScheduler}
@@ -16,21 +17,21 @@ import zio.interop.catz._
 
 class PlayModule extends AbstractModule {
   override def configure(): Unit = {
-    /*
-        implicit val contextShift: ContextShift[IO] = IO.contextShift(global)
 
-        bind(new TypeLiteral[BasicActionLang[IO, Employee]] {}).toInstance(DoobieActionLang())
-        bind(new TypeLiteral[BasicActionLang[IO, Department]] {}).toInstance(DoobieActionLang())
-        bind(new TypeLiteral[MatLang[IO]] {}).toInstance(IOMatLang())
-        bind(new TypeLiteral[Monad[IO]] {}).toInstance(implicitly[Monad[IO]])
-        */
-    /*
-    implicit val contextShift: ContextShift[Task] = Task.contextShift
+    implicit val contextShiftIO: ContextShift[IO] = IO.contextShift(globalContext)
+
+    bind(new TypeLiteral[BasicActionLang[IO, Employee]] {}).toInstance(DoobieActionLang())
+    bind(new TypeLiteral[BasicActionLang[IO, Department]] {}).toInstance(DoobieActionLang())
+    bind(new TypeLiteral[MatLang[IO]] {}).toInstance(IOMatLang())
+    bind(new TypeLiteral[Monad[IO]] {}).toInstance(implicitly[Monad[IO]])
+
+
+    implicit val contextShiftTask: ContextShift[Task] = Task.contextShift
 
     bind(new TypeLiteral[BasicActionLang[Task, Employee]] {}).toInstance(DoobieActionLang())
     bind(new TypeLiteral[BasicActionLang[Task, Department]] {}).toInstance(DoobieActionLang())
     bind(new TypeLiteral[MatLang[Task]] {}).toInstance(TaskMatLang(globalScheduler))
-    bind(new TypeLiteral[Monad[Task]] {}).toInstance(implicitly[Monad[Task]])*/
+    bind(new TypeLiteral[Monad[Task]] {}).toInstance(implicitly[Monad[Task]])
 
     bind(new TypeLiteral[BasicActionLang[zio.Task, Employee]] {}).toInstance(DoobieActionLang())
     bind(new TypeLiteral[BasicActionLang[zio.Task, Department]] {}).toInstance(DoobieActionLang())
